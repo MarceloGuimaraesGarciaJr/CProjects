b5f008b051f32f3f5f496dfd5a3c2be159c3cf0a
Marcelo Garcia <marceloggarciajr@gmail.com>
2022-09-06 15:49:57 -0300

    Programa para ligar relé

--- Reles/src/main.cpp
+++ Reles/src/main.cpp
@@ -1,8 +1,12 @@
 #include <Arduino.h>
 void setup() {
+pinMode(27, OUTPUT);
 
 }
 
 void loop() {
-// write your code here
+    digitalWrite(27, LOW);
+    delay(2000);
+    digitalWrite(27,HIGH);
+    delay (1000);
 }
